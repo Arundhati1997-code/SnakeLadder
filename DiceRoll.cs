f886b1366a798fae16504bd6760502453ec7a930
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-14 00:12:14 +0530

    Add files via upload

---
+++ DiceRoll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class DiceRoll
+    {
+        public static void DiceRolled()
+        {
+            //for (int i = 0; i < 200; i++)
+            {
+                Random randomNumber = new Random();
+                var diceRoll = randomNumber.Next(1, 7);
+                Console.WriteLine("Dice rolled:{0}", diceRoll);
+
+            }
+        }
+    }
+}
