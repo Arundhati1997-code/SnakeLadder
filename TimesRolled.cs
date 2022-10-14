f1e749e5d8c92bab4a68a22b96a3eacf6e6cb7c2
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-14 09:57:42 +0530

    Add files via upload

---
+++ TimesRolled.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class TimesRolled
+    {
+        public static void Rolled()
+        {
+            var playerPosition = 0;
+            var timeRolled = 0;
+
+            do
+            {
+                string[] turnOutcomes = { "noPlay", "Ladder", "Snake" };
+                Random randomNumber = new Random();
+                var diceRoll = randomNumber.Next(1, 7);
+                timeRolled = timeRolled + 1;
+                var turn = randomNumber.Next(0, 2);
+                var turnResult = turnOutcomes[turn];
+                if (playerPosition == 100)
+                {
+                    Console.WriteLine("Number of times the dice rolled:{0}", timeRolled);
+                    break;
+                }
+                switch (turnResult)
+                {
+                    case "Ladder":
+                        if (!((playerPosition + diceRoll) > 100))
+                        {
+                            playerPosition = playerPosition + diceRoll;
+                        }
+                        break;
+                    case "Snake":
+                        if (playerPosition - diceRoll < 0)
+                        {
+                            playerPosition = 0;
+                        }
+                        else
+                        {
+                            playerPosition = playerPosition - diceRoll;
+                        }
+                        break;
+                    default:
+                    case "noPlay":
+                        break;
+                }
+                Console.WriteLine("Dice Rolled:{0},turn:{1},playerPosition:{2},turnresult:{3}", diceRoll, turn, playerPosition, turnResult);
+            }
+            while (playerPosition <= 100);
+        }
+    }
+}
