0f552738242482a4bead0c30f7331d488c3185ba
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-14 09:59:58 +0530

    Add files via upload

---
+++ PlayerPostion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class PlayerPostion
+    {
+        public void PlayerPosition()
+        {
+            var playerPosition = 0;
+            Console.WriteLine("The position of the player is at:{0}", playerPosition);
+        }
+    }
+}
+
+
