21c3540895765e1f11e6ca8b459be0d7f9573982
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-14 09:52:40 +0530

    Add files via upload

---
+++ TurnedOutcomes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public static class TurnedOutcomes
+    {
+        public static void Outcome()
+        {
+            string[] turnOutcomes = { "noPlay", "Ladder", "Snake" };
+            Random randomNumber = new Random();
+            var diceRoll = randomNumber.Next(1, 7);
+            var turn = randomNumber.Next(0, 2);
+            var playerPosition = 0;
+            var turnResult = turnOutcomes[turn];
+            switch (turnResult)
+            {
+                case "Ladder":
+                    playerPosition = playerPosition + diceRoll;
+                    break;
+                case "Snake":
+                    playerPosition = playerPosition - diceRoll;
+                    break;
+                default:
+                case "noPlay":
+                    break;
+            }
+            Console.WriteLine("Dice Rolled:{0},turn:{1},playerPosition:{2},turnresult:{3}", diceRoll, turn, playerPosition, turnResult);
+        }
+    }
+}
